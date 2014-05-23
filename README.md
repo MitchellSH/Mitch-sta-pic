05244d4980de7bae4fc1cdbd7cc5586d28e85efd
Mitchell Hollander <MitchellSH@live.com>
2014-05-23 11:55:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Mitch-Sta-Pic
+=============
+
+A refactoring of my Tagstapic app with a new design, layout, and better functionality.
+
+
+Purpose
+=============
+
+A fun and simple app that uses Instagram's API to pull in pictures associated with the #tag you have searched. After
+you have searched for pictures, you can then select up to 6 pictures and create a slideshow with your selection. Fun!
+
+
+Dependencies
+=============
+
+* HTML5
+* CSS3
+* Javascript
+* jQuery
+* Lo_Dash
+* Bootstrap
+
+
+URL
+=============
+
+www.mitchellhollander.com/Mitch-sta-pic
---
+++ js/source/main.js
@@ -0,0 +1,133 @@
+(function(){
+
+  'use strict';
+
+  $(document).ready(initialize);
+
+  function initialize(){
+    $('#start').click(showSearchSection);
+    $('.btn').click(getPics);
+    // $('#reset').click(homeButton);
+//     $('#invert').click(invertPic);
+//     $('#sepia').click(sepiaPic);
+//     $('#gray').click(grayscalePic);
+//     $('#crazy').click(gangnam);
+//     $('#rock').click(rockN);
+    // $(document).on('click','img',add2Q);
+    $('#search-bar').keypress(function(e){
+      if(e.keyCode===13){
+        getPics();
+      }
+    });
+  };
+//
+//
+//   //------Minor Show and Hide Functions--------
+//
+//
+  function showSearchSection(){
+    $('.search-container').show().addClass('animated fadeIn');
+    // $('#container3').show();
+    $('#title').text('Search #Tags').addClass('animated slideInDown');
+    $('#title-p').text('Search and Select 6 Pics.').addClass('animated fadeIn');
+    $('.start').hide();
+  }
+
+//   function homeButton(){
+//     emptyPics();
+//     $('#Q-box').empty();
+//     $('#title').text('#Tagstapic');
+//     $('#title-p').text('');
+//     $('#container2').hide();
+//     $('#container3').hide();
+//     $('#search-box').val('');
+//     $('.orbitSlider').empty();
+//   }
+//
+  function emptyPics(){
+    $('#target').empty();
+
+  }
+//
+//
+//   //------Minor Show and Hide Functions--------
+//
+//   //------Music Functions----------------
+//
+//
+//   function rockN(){
+//     document.getElementById('black').play();
+//     document.getElementById('gang').pause();
+//   }
+//
+//   function gangnam(){
+//     document.getElementById('gang').play();
+//     document.getElementById('black').pause();
+//   }
+//
+//
+//
+//
+//
+//   //------Music Functions----------------
+//
+//   //------Filter Functions---------------
+//
+//   function invertPic(){
+//     $('.orbitSlider img').removeClass();
+//     $('.orbitSlider img').addClass('inverted');
+//
+//   }
+//
+//   function grayscalePic(){
+//     $('.orbitSlider img').removeClass();
+//     $('.orbitSlider img').addClass('grayscale');
+//
+//   }
+//
+//   function sepiaPic(){
+//     $('.orbitSlider img').removeClass();
+//     $('.orbitSlider img').addClass('sepia');
+//
+//   }
+//
+//   //------Filter Functions---------------
+//
+//   //------Adding To Q Functions----------
+//
+//   function add2Q(){
+//     if($('#Q-box').children('img').length < 6 || !$('#Q-box').children('img').length){
+//       $('#Q-box').append(this);
+//       var $li = $('<li>');
+//       var $bigPic = $(this).clone().attr('src');
+//       $bigPic = $bigPic.replace('_5','_6');
+//       $li.append('<img src="' +$bigPic+ '">');
+//       $('.orbitSlider').append($li);
+//     }
+//   }
+//   //------Adding To Q Functions----------
+//
+//   //------Instagram API Functions--------
+//
+//
+  function getPics(){
+    emptyPics();
+    var tag = $('#search-bar').val();
+    var count = 6;
+    var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=86cecf102fdd46599736c4deac452424&callback=?&count=' + count;
+    $.getJSON(url, displayPics);
+  }
+//
+  function displayPics(instaData){
+    var pics = instaData.data;
+    for(var i=0; i < pics.length; i++){
+      $('#target').append('<img src="'+pics[i].images.thumbnail.url+'">');
+    }
+  }
+//
+//   //------Instagram API Functions--------
+//
+//
+//
+//
+})();
